4441e0c0f5ef56a139ff69cba74498271b793ab4
Alvaro Laserna <alvarolaserna@MacBook-Pro-2.local>
2026-02-05 17:14:28 +0200

    fix agent functions

--- agent_func.py
+++ agent_func.py
@@ -7,10 +7,10 @@ import datetime
 import requests
 import subprocess
 
-def give_time():
+def give_time(_run_test_id='1') -> str:
     """returns the current date"""
     return datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
 
-def give_name():
+def give_name(_run_test_id='1'):
     """returns the your name"""
     return "Barko Agent 007"
